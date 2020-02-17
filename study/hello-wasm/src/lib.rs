ff3d3cdd10781fc060f075d265640cfd0926f488
gongbaodd@Inspiron_Ubuntu <gongbaodd@qq.com>
2020-02-17 20:22:21 +0800

    hello-wasm

--- study/hello-wasm/src/lib.rs
+++ study/hello-wasm/src/lib.rs
@@ -14,6 +14,6 @@ extern {
 }
 
 #[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, hello-wasm!");
+pub fn greet(name: &str) {
+    alert(&format!("Hello, {}", name));
 }
---
+++ study/hello-wasm/www/index.js
@@ -0,0 +1,3 @@
+import * as wasm from "hello-wasm-pack";
+
+wasm.greet("宫健");
